9a78ba247b566bdea906a685b79da373d24d4535
ruyriy <74980038+ruyriy@users.noreply.github.com>
2024-03-26 23:57:59 +0200

    script.js

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-﻿document.getElementById('dataForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var inputData = document.getElementById('dataInput').value;
-    sendData(inputData);
-});
+﻿window.onload = function() {
+    fetchData();
+};
 
-function sendData(data) {
+function fetchData() {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
-                document.getElementById('output').innerText = xhr.responseText;
+                document.getElementById('dataOutput').innerHTML = xhr.responseText;
             } else {
                 console.error('Error:', xhr.statusText);
             }
         }
     };
-    xhr.open('POST', 'server.php', true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.send('data=' + encodeURIComponent(data));
+    xhr.open('GET', 'server.php', true);
+    xhr.send();
 }
